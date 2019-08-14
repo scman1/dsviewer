3d83e6c77bd4ed64382253b4a181f12e42cb876d
Abraham Nieva <nievadelahidalgaa@cardiff.ac.uk>
2019-08-14 01:13:56 +0100

    pointed root to specimens index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+    root 'specimens#index'
     resources :specimens, :id => /[A-Za-z0-9\.]+/
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
