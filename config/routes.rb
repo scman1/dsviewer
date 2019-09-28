21d3f452869603296c52c307c4c611c729c22a20
Abraham Nieva <a_nieva@hotmail.com>
2019-09-28 15:23:32 +0100

    fix for accepting specimen id's with underscores

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
     root 'specimens#index'
-    resources :specimens, :id => /[A-Za-z0-9\.]+/
+    resources :specimens, :id => /[A-Za-z0-9\.\_]+/
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
