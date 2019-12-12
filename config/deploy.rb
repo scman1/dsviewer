92ad9833009c987818245ba6b985c14ecfeb48a8
Francisco Quevedo <quevedofernandezf@cardiff.ac.uk>
2019-12-12 09:25:42 +0000

    Allow Capistrano to connect to the production server using ssh. During the deployment do the following tasks:  - Get the code form github  - Run bundler install to install/update any new gem  - Generate assets locally and copy to the server.  - Run migrations  - Restart passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,75 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "dsviewer"
+set :repo_url, "https://github.com/scman1/dsviewer.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# Deploy to the user's home directory
+set :deploy_to, "/home/deploy/#{fetch :application}"
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
 
-# Default value for :pty is false
-# set :pty, true
+# Clear existing task so we can replace it rather than "add" to it.
+Rake::Task["deploy:compile_assets"].clear
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+namespace :deploy do
+  desc 'Compile assets'
+  task :compile_assets => [:set_rails_env] do
+    # invoke 'deploy:assets:precompile'
+    invoke 'deploy:assets:copy_manifest'
+    invoke 'deploy:assets:precompile_local'
+    invoke 'deploy:assets:backup_manifest'
+  end
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+  namespace :assets do
+    local_dir = "./public/assets/"
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+    # Download the asset manifest file so a new one isn't generated. This makes
+    # the app use the latest assets and gives Sprockets a complete manifest so
+    # it knows which files to delete when it cleans up.
+    desc 'Copy assets manifest'
+    task copy_manifest: [:set_rails_env] do
+      on roles(fetch(:assets_roles, [:web])) do
+        remote_dir = "#{fetch(:ssh_user)}@#{host.hostname}:#{shared_path}/public/assets/"
+        ssh_key = "#{fetch(:ssh_key)}"
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
+        run_locally do
+          begin
+            execute "mkdir -p #{local_dir}"
+            execute "scp -i #{ssh_key} '#{remote_dir}.sprockets-manifest-*' #{local_dir}"
+          rescue
+            # no manifest yet
+          end
+        end
+      end
+    end
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+    desc "Precompile assets locally and then rsync to web servers"
+    task :precompile_local do
+      # compile assets locally
+      run_locally do
+        execute "bundle exec rake assets:precompile"
+      end
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+      # rsync to each server
+      on roles(fetch(:assets_roles, [:web])) do
+        # this needs to be done outside run_locally in order for host to exist
+        remote_dir = "#{fetch(:ssh_user)}@#{host.hostname}:#{shared_path}/public/assets"
+        ssh_key = "#{fetch(:ssh_key)}"
+
+        run_locally do
+          execute "rsync -e 'ssh -i #{ssh_key}' -av #{local_dir} #{remote_dir}"
+        end
+      end
+
+      # clean up
+      run_locally { execute "rm -rf #{local_dir}" }
+    end
+  end
+end
